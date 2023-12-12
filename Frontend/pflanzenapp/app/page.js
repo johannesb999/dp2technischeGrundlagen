7c3d269cd0d1ef2f3dd75e416dd283ef6825cc1b
Johannes Biess <128609732+johannesb999@users.noreply.github.com>
2023-12-12 12:37:34 +0100

    update
    scheint zu funktionieren

---
+++ Frontend/pflanzenapp/app/page.js
@@ -0,0 +1,13 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useStae, useEffect } from "react";
+import axios from "axios";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>Hallo</div>
+    </main>
+  );
+}
