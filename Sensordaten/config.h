50402c75fa437368f6f7fde4d5e84d1eb83c6bd8
Johannes Biess <128609732+johannesb999@users.noreply.github.com>
2023-12-17 17:04:52 +0100

    update

--- Sensordaten/config.h
+++ Sensordaten/config.h
@@ -1,16 +1,28 @@
+// config.h
 #ifndef CONFIG_H
 #define CONFIG_H
 
-extern const char* ssid;
-extern const char* password;
-extern const char* mqtt_server;
-extern const int mqtt_port;
-extern const char* values_topic;
+// WiFi credentials
+const char* ssid = "JB hotspot 23U";
+const char* password = "12345679";
 
-// Sensor Pins
-#define SOIL_MOISTURE_PIN 32
-#define LDR_PIN 33
-#define DHTPIN 14
-#define DHTTYPE DHT22
+// const char* ssid = "YOUR_WIFI";
+// const char* password = "YOUR_PASSWORD";
+
+// MQTT Broker settings
+const char* mqtt_server = "mqtt.hfg.design";
+const int mqtt_port = 1883;
+
+// MQTT Topics
+
+const char* values_topic = "dp2/values";
+
+// Intervall
+#define TEMP_READ_INTERVAL 300
+#define HUMIDITY_READ_INTERVAL 600 
+#define SOIL_MOISTURE_READ_INTERVAL 300
+#define LIGHT_READ_INTERVAL 200
+
+
 
 #endif // CONFIG_H
