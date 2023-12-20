6d9ca603ba95b39142aecff0ee46292c0298b65a
Johannes Biess <128609732+johannesb999@users.noreply.github.com>
2023-12-20 13:03:28 +0100

    update
    to avoid mergeconflicts

--- Userservice/index.js
+++ Userservice/index.js
@@ -1,65 +1,65 @@
-require('dotenv').config();
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI}`;
 const mongoClient = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 // Verbinden mit der Datenbank
 async function connectToDatabase() {
-    await mongoClient.connect();
-    return mongoClient.db("userDB").collection("users");
+  await mongoClient.connect();
+  return mongoClient.db("userDB").collection("users");
 }
 
 // Endpunkt für die Registrierung
-app.post('/register', async (req, res) => {
-    const users = await connectToDatabase();
+app.post("/register", async (req, res) => {
+  const users = await connectToDatabase();
 
-    const userData = {
-        username: req.body.username,
-        password: req.body.password
-    };
-    console.log(userData);
+  const userData = {
+    username: req.body.username,
+    password: req.body.password,
+  };
+  console.log(userData);
 
-    const existingUser = await users.findOne({ username: userData.username });
-    if (existingUser) {
-        console.log(`Benutzer ${userData.username} existiert bereits`);
-        return res.status(400).send('Benutzer existiert bereits');
-    }
+  const existingUser = await users.findOne({ username: userData.username });
+  if (existingUser) {
+    console.log(`Benutzer ${userData.username} existiert bereits`);
+    return res.status(400).send("Benutzer existiert bereits");
+  }
 
-    await users.insertOne(userData);
-    res.status(201).send('Benutzer erfolgreich registriert');
-    console.log(`Benutzer ${userData.username} wurde registriert`);
+  await users.insertOne(userData);
+  res.status(201).send("Benutzer erfolgreich registriert");
+  console.log(`Benutzer ${userData.username} wurde registriert`);
 });
 
 // Endpunkt für Login
-app.post('/login', async (req, res) => {
-    const users = await connectToDatabase();
-    const { username, password } = req.body;
+app.post("/login", async (req, res) => {
+  const users = await connectToDatabase();
+  const { username, password } = req.body;
 
-    const user = await users.findOne({ username });
-    if (!user || user.password !== password) {
-        return res.status(401).send('Ungültige Anmeldedaten');
-    }
+  const user = await users.findOne({ username });
+  if (!user || user.password !== password) {
+    return res.status(401).send("Ungültige Anmeldedaten");
+  }
 
-    res.status(200).send('Erfolgreich angemeldet');
+  res.status(200).send("Erfolgreich angemeldet");
 });
 
 // Endpunkt für Logout
-app.post('/logout', (req, res) => {
-    res.status(200).send('Erfolgreich abgemeldet');
+app.post("/logout", (req, res) => {
+  res.status(200).send("Erfolgreich abgemeldet");
 });
 
 const PORT = process.env.USERSERVICE_PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server läuft auf Port ${PORT}`);
+  console.log(`Server läuft auf Port ${PORT}`);
 });
---
+++ frontend/src/Register.svelte
@@ -0,0 +1,32 @@
+<script>
+    import axios from 'axios';
+
+    let username = '';
+    let password = '';
+
+    async function registerUser() {
+        try {
+            const response = await axios.post('http://localhost:3001/register', {
+                username,
+                password
+            });
+            console.log('Registrierung erfolgreich:', response);
+            // Hier können Sie weitere Aktionen nach erfolgreicher Registrierung hinzufügen
+        } catch (error) {
+            console.error('Fehler bei der Registrierung:', error);
+            // Behandeln von Fehlern, z.B. Benutzer existiert bereits
+        }
+    }
+</script>
+
+<form on:submit|preventDefault={registerUser}>
+    <div>
+        <label for="username">Benutzername:</label>
+        <input id="username" type="text" bind:value={username} />
+    </div>
+    <div>
+        <label for="password">Passwort:</label>
+        <input id="password" type="password" bind:value={password} />
+    </div>
+    <button type="submit">Registrieren</button>
+</form>
