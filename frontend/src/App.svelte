54e12f86cf8e880b4da896f086356034924d8fa6
JannikFlaig <128989434+JannikFlaig@users.noreply.github.com>
2023-12-20 12:54:36 +0100

    frontend(vite)

---
+++ frontend/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Register from "./Register.svelte";
+  import Login from "./Login.svelte";
+  import Router from 'svelte-spa-router';
+
+
+  const routes= {
+    "/": Login,
+    "/Login": Login,
+    "/Register": Register
+  };
+</script>
+
+
+
+<main>
+  <nav>
+    <a href='#/Login'>LOGIN</a>
+    <a href='#/Register'>Register</a>
+  </nav>
+  <h1>Joahhes ist Lost</h1>
+  <Router {routes}></Router>
+</main>
+
+
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
